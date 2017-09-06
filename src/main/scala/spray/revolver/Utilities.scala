486be0266423c966ceb2c801eaa818489cad0fe5
andrew <uandrew1965@gmail.com>
2017-09-06 13:06:19 +0300

    .

--- src/main/scala/spray/revolver/Utilities.scala
+++ src/main/scala/spray/revolver/Utilities.scala
@@ -20,49 +20,53 @@ import sbt._
 import scala.Console._
 
 object Utilities {
-  def colorLogger(state: State): Logger = colorLogger(state.log)
+    def colorLogger(state: State): Logger = colorLogger(state.log)
 
-  def colorLogger(logger: Logger): Logger = new Logger {
-    def trace(t: => Throwable) { logger.trace(t) }
-    def success(message: => String) { success(message) }
-    def log(level: Level.Value, message: => String): Unit =
-      logger.log(level, colorize(logger.ansiCodesSupported, message))
-  }
+    def colorLogger(logger: Logger): Logger = new Logger {
+        def trace(t: => Throwable) {
+            logger.trace(t)
+        }
+        def success(message: => String) {
+            success(message)
+        }
+        def log(level: Level.Value, message: => String): Unit =
+            logger.log(level, colorize(logger.ansiCodesSupported, message))
+    }
 
-  val simpleColors =
-    Seq(
-      "RED" -> RED,
-      "GREEN" -> GREEN,
-      "YELLOW" -> YELLOW,
-      "BLUE" -> BLUE,
-      "MAGENTA" -> MAGENTA,
-      "CYAN" -> CYAN,
-      "WHITE" -> WHITE
-    )
-  val rgbColors = (0 to 255) map rgb
-  val ansiTagMapping: Seq[(String, String)] =
-    (
-      Seq(
-        "BOLD" -> BOLD,
-        "RESET" -> RESET
-      ) ++
-      simpleColors ++
-      simpleColors.map(reversed) ++
-      simpleColors.map(underlined) ++
-      rgbColors
-    ).map(delimited("[", "]"))
+    val simpleColors =
+        Seq(
+            "RED" -> RED,
+            "GREEN" -> GREEN,
+            "YELLOW" -> YELLOW,
+            "BLUE" -> BLUE,
+            "MAGENTA" -> MAGENTA,
+            "CYAN" -> CYAN,
+            "WHITE" -> WHITE
+        )
+    val rgbColors = (0 to 255) map rgb
+    val ansiTagMapping: Seq[(String, String)] =
+        (
+          Seq(
+              "BOLD" -> BOLD,
+              "RESET" -> RESET
+          ) ++
+            simpleColors ++
+            simpleColors.map(reversed) ++
+            simpleColors.map(underlined) ++
+            rgbColors
+          ).map(delimited("[", "]"))
 
-  def reversed(color: (String, String)): (String, String) =
-    ("~"+color._1) -> (color._2+REVERSED)
-  def underlined(color: (String, String)): (String, String) =
-    ("_"+color._1) -> (color._2+UNDERLINED)
-  def delimited(before: String, after: String)(mapping: (String, String)): (String, String) =
-    (before+mapping._1+after, mapping._2)
-  def rgb(idx: Int): (String, String) = ("RGB"+idx, "\u001b[38;5;"+idx+"m")
+    def reversed(color: (String, String)): (String, String) =
+        ("~" + color._1) -> (color._2 + REVERSED)
+    def underlined(color: (String, String)): (String, String) =
+        ("_" + color._1) -> (color._2 + UNDERLINED)
+    def delimited(before: String, after: String)(mapping: (String, String)): (String, String) =
+        (before + mapping._1 + after, mapping._2)
+    def rgb(idx: Int): (String, String) = ("RGB" + idx, "\u001b[38;5;" + idx + "m")
 
-  def replaceAll(message: String, replacer: String => String) =
-    ansiTagMapping.foldLeft(message)((msg, tag) => msg.replaceAll(java.util.regex.Pattern.quote(tag._1), replacer(tag._2)))
+    def replaceAll(message: String, replacer: String => String) =
+        ansiTagMapping.foldLeft(message)((msg, tag) => msg.replaceAll(java.util.regex.Pattern.quote(tag._1), replacer(tag._2)))
 
-  def colorize(ansiCodesSupported: Boolean, message: String): String =
-    replaceAll(message, if (ansiCodesSupported) identity else _ => "")
+    def colorize(ansiCodesSupported: Boolean, message: String): String =
+        replaceAll(message, if (ansiCodesSupported) identity else _ => "")
 }
